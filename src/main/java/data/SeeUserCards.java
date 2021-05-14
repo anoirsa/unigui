a5b036583a88d1076d0df0c3c96d48d96c762e79
anoirsa <anouarbelilawoo@outlook.com>
2021-05-14 14:25:18 +0300

    Adjusting the seecards layout

--- src/main/java/data/SeeUserCards.java
+++ src/main/java/data/SeeUserCards.java
@@ -2,9 +2,13 @@ package data;
 
 import data.enums.Purpose;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -19,9 +23,14 @@ public class SeeUserCards {
 
     Stage window;
     List<ImageView> listOfImages;
+    int howManyCardsLeft;
 
     public Integer MainSeeCards(List<String> listToShow , Purpose purpose) {
         AtomicInteger valueReturnd = new AtomicInteger();
+
+        //Determining how many cards are left based on the size of the argument "listToShow"
+        howManyCardsLeft = listToShow.size();
+        //
         listOfImages = new ArrayList<>();
         window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
@@ -73,10 +82,35 @@ public class SeeUserCards {
         gridPane.setVgap(10);
         gridPane.setHgap(10);
         gridPane.setPadding(new Insets(10,10,10,10));
-        Scene scene = new Scene(gridPane,450,450);
+       // Scene scene = new Scene(gridPane,450,450);
+       // Pre-Testing
+       Scene scene = new Scene(returnedLayout(gridPane,purpose),450,450);
         window.setScene(scene);
         window.showAndWait();
         return valueReturnd.get();
+    }
+
+    BorderPane returnedLayout(GridPane gridPane , Purpose purpose) {
+        Label headerLabel = new Label();
+        Label footerLabel = new Label();
+        BorderPane borderPane = new BorderPane();
+        gridPane.setAlignment(Pos.CENTER);
+        headerLabel.setAlignment(Pos.TOP_CENTER);
+        borderPane.setTop(headerLabel);
+        borderPane.setCenter(gridPane);
+        switch (purpose) {
+            case COMPUTER_SEE:
+                headerLabel.setText("The computer cards are hidden and cannot be seen");
+                footerLabel.setAlignment(Pos.BOTTOM_CENTER);
+                borderPane.setBottom(footerLabel);
+                footerLabel.setText("Cards left are :" + howManyCardsLeft);
+                break;
+            case CHOOSE_PURPOSE:
+                headerLabel.setText("You need to choose one of these options you have.");
+            case SEE_PURPOSE:
+                headerLabel.setText("Here you can see all your cards");
+        }
+        return borderPane;
     }
 
 
